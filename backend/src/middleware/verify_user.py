f8f8d235701e3bbb272e883be1869095cb38d5d9
balagurubarann <balagurubaran2002@gmail.com>
2026-01-11 18:02:40 +0530

    verification middleware added

--- backend/src/middleware/verify_user.py
+++ backend/src/middleware/verify_user.py
@@ -1,1 +1,47 @@
 # USER VERIFICATION
+
+from functools import wraps
+from src.utils import Level
+from flask import jsonify, g
+from flask_jwt_extended import get_jwt_identity, verify_jwt_in_request
+from src.model import Staff
+
+staffLevel = {
+    "L1": 1,
+    "L2": 2,
+    "L3": 3
+}
+
+def least_staff_required(level: Level):
+
+    def wrapper(fn):
+
+        @wraps(fn)
+        def decorator(*args, **kwargs):
+
+            verify_jwt_in_request()
+            staffId = get_jwt_identity()
+
+            staff = Staff.query.get(id=staffId)
+
+            if not staff:
+
+                return jsonify({
+                    "message": "Staff not found! - Login required!",
+                    "success": False
+                }), 404
+
+            if staffLevel[staff.level] < staffLevel[level]:
+
+                return jsonify({
+                    "message": f"Staff level must be atleast {level}",
+                    "success": False
+                }), 401
+            
+            g.current_staff = staff
+
+            return fn(*args, **kwargs)
+
+        return decorator
+    
+    return wrapper
